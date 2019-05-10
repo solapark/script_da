946b99f7e3ef8af31ea30e0294ab5d9e196049bd
sola <opop0628@gmail.com>
2019-05-10 18:22:02 +0900

    visual modified

--- yolo/plot_thresh_result.py
+++ yolo/plot_thresh_result.py
@@ -3,6 +3,7 @@ import sys
 import matplotlib.pyplot as plt
 import sys
 import re
+import numpy as np
 
 def get_network_name_from_path(file_path):
 #file_path = "pathToFile/thresh_result_networkName_with_networkName.log"
@@ -59,20 +60,24 @@ def get_target_list(result_list, target):
     return target_list
 
 def plot_TP_FP_bar(ax, thresh_list, TP_list, FP_list):
-    width = 0.05
-    ax.bar(thresh_list, TP_list, color = 'r', width = width, label = 'TP', bottom = FP_list)
-    ax.bar(thresh_list, FP_list, color = 'b', width = width, label = 'FP')
+    width = 0.01
+    ax.bar(thresh_list, TP_list, color = 'lightgreen', width = width, label = 'TP', bottom = FP_list)
+    ax.bar(thresh_list, FP_list, color = 'lightcoral', width = width, label = 'FP')
     ax.set_ylabel('TP, FP', rotation=0, labelpad=20)
+    TP_FP = [TP+FP for TP, FP in zip(TP_list, FP_list)]
+    ax.set_ylim(0, max(TP_FP))
+    #ax.set_yticks(np.arange(0, max(TP_FP), 1))
     ax.legend()
 
 def plot_TP_FP_ratio(ax, thresh_list, TP_list, FP_list, title):
     if(title == 'TP/FP'):
         TP_FP_ratio = [ float(TP) / float(FP) if FP != 0 else 0 for TP, FP in zip(TP_list, FP_list) ]
-        ax.plot(thresh_list, TP_FP_ratio, label = title)
+        ax.plot(thresh_list, TP_FP_ratio, label = title, color = 'k')
     elif(title == 'FP/TP'):
         TP_FP_ratio = [ float(FP) / float(TP) if TP != 0 else 0 for TP, FP in zip(TP_list, FP_list) ]
-        ax.plot(thresh_list, TP_FP_ratio, label = title)
+        ax.plot(thresh_list, TP_FP_ratio, label = title, color = 'k')
     ax.set_ylabel(title, rotation=0, labelpad=20)
+    ax.set_ylim(0, max(TP_FP_ratio))
     ax.legend()
 
 def plot_result(ax, thresh_list, result_list, title):
@@ -81,13 +86,18 @@ def plot_result(ax, thresh_list, result_list, title):
 
     plot_TP_FP_bar(ax, thresh_list, TP, FP)
 
-    ratio_ax = ax.twinx()
-    if(title == 'result 0 to thresh'):
-        plot_TP_FP_ratio(ratio_ax, thresh_list, TP, FP, 'FP/TP')
-    elif(title == 'result thresh to 1'):
-        plot_TP_FP_ratio(ratio_ax, thresh_list, TP, FP, 'TP/FP')
     ax.set_xlabel('threshold')
+    ax.set_xticks(np.arange(0, 1.01, 0.1))
+    ax.set_xlim(0, 1)
     ax.set_title(title)
+
+    ratio_ax = ax.twinx()
+    if(title == '0 ~ thresh'):
+        plot_TP_FP_ratio(ratio_ax, thresh_list, TP, FP, 'FP/TP')
+    elif(title == 'thresh ~ 1'):
+        plot_TP_FP_ratio(ratio_ax, thresh_list, TP, FP, 'TP/FP')
+
+    ratio_ax.grid()
 
 def main(argv):
     file_path = argv[1]
@@ -98,8 +108,8 @@ def main(argv):
     thresh_list, result_list = extract_thresh_result(file_path)
     reuslt_0_to_target_list = get_result_from_0_to_target(result_list)
 
-    plot_result(result_0_to_target_ax, thresh_list, reuslt_0_to_target_list, 'result 0 to thresh')
-    plot_result(result_target_to_1_ax, thresh_list, result_list, 'result thresh to 1')
+    plot_result(result_0_to_target_ax, thresh_list, reuslt_0_to_target_list, '0 ~ thresh')
+    plot_result(result_target_to_1_ax, thresh_list, result_list, 'thresh ~ 1')
 
     whole_title = get_network_name_from_path(file_path)
     plt.suptitle(whole_title)
