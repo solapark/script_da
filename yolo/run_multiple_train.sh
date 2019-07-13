2b7f75bebfefd71d4b44c675fee2a080c446a8dd
sola <opop0628@gmail.com>
2019-07-13 16:37:35 +0900

    run_multiple_train

---
+++ yolo/run_multiple_train.sh
@@ -0,0 +1,12 @@
+set -x
+
+gpu_num=$1
+ARGS=("$@")
+class_num=$#
+
+for (( c =1; c< $class_num; c++))
+do
+	class=${ARGS[$c]}
+	#echo "c=$c class=$class class_num=$class_num gpu_num=$gpu_num"
+	./darknet detector train VOC/VOC_$calss.data VOC/VOC_one_class.cfg /data1/sap/backup/darknet53.conv.74 -dont_show -gpus $gpu_num > VOC/loss/loss_VOC_$class.log	
+done
